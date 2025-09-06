902423b546b3cbd03031c4a65c6f28a443253fca
Keshee <sanicyll@gmail.com>
2025-09-06 23:27:28 +0300

    feat: added some visual effects to slider. Addes some global classes

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,14 +1,17 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Spin } from "antd";
 
 export default function Home() {
   const [dogData, setDogData] = useState([]);
   const [currentX, setCurrentX] = useState(0);
-  const maxDogPictures = 5;
+  const [loading, setLoading] = useState(false);
+  const maxDogPictures = 10;
   const cardWidth = 320; // Width of each card including margin
 
   useEffect(() => {
+    setLoading(true);
     const headers = new Headers({
       "Content-Type": "application/json",
       "x-api-key": "DEMO-API-KEY",
@@ -35,11 +38,12 @@ export default function Home() {
         console.log(enrichedData);
         setDogData(enrichedData);
       })
-      .catch((error) => console.log("error", error));
+      .catch((error) => console.log("error", error))
+      .finally(() => setLoading(false));
   }, []);
 
   const handleScrollForward = () => {
-    if (currentX < (maxDogPictures - 4) * cardWidth) {
+    if (currentX < (maxDogPictures - 5) * cardWidth) {
       setCurrentX(currentX + cardWidth);
     }
   };
@@ -58,45 +62,52 @@ export default function Home() {
   };
 
   return (
-    <div className="relative w-full max-w-[60%] mx-auto">
-      <div>
-        <button
-          className="absolute left-0 top-1/2 -translate-y-1/2 bg-white px-3 py-2 shadow rounded-full z-50"
-          onClick={handleScrollBack}
-        >
-          ◀
-        </button>
-        <button
-          className="absolute right-0 top-1/2 -translate-y-1/2 bg-white px-3 py-2 shadow rounded-full z-50"
-          onClick={handleScrollForward}
-        >
-          ▶
-        </button>
-      </div>
+    <Spin spinning={loading} tip="Loading data...">
+      <div className="relative max-h-[288px] w-full max-w-[60%] mx-auto">
+        <div>
+          <button className="left-0 slider-button" onClick={handleScrollBack}>
+            ◀
+          </button>
+          <button
+            className="right-0 slider-button"
+            onClick={handleScrollForward}
+          >
+            ▶
+          </button>
+        </div>
 
-      <div className="overflow-hidden">
-        <div
-          className="flex gap-4 px-4 py-6 transition-transform duration-500 ease-in-out"
-          style={{
-            transform: `translateX(-${currentX}px)`,
-          }}
-        >
-          {dogData.map((dog) => (
-            <div className="text-center flex-shrink-0 w-72" key={dog.id}>
-              <div className="w-72 h-72 rounded-full overflow-hidden shadow-lg">
-                <img
-                  src={`https://cdn2.thedogapi.com/images/${dog.reference_image_id}.jpg`}
-                  alt={dog?.name || "Dog"}
-                  className="w-full h-full object-cover"
-                />
+        <div className="relative overflow-hidden">
+          <div
+            className="left-0 side-blur"
+          ></div>
+
+          <div
+            className="right-0 bg-gradient-to-l side-blur"
+          ></div>
+
+          <div
+            className="flex gap-4 px-10 py-6 transition-transform duration-500 ease-in-out"
+            style={{
+              transform: `translateX(-${currentX}px)`,
+            }}
+          >
+            {dogData.map((dog) => (
+              <div className="text-center flex-shrink-0 w-72" key={dog.id}>
+                <div className="w-72 h-72 rounded-full overflow-hidden shadow-lg">
+                  <img
+                    src={`https://cdn2.thedogapi.com/images/${dog.reference_image_id}.jpg`}
+                    alt={dog?.name || "Dog"}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <p className="mt-4 text-lg font-semibold">
+                  {dog?.name || "Unknown"}
+                </p>
               </div>
-              <p className="mt-4 text-lg font-semibold">
-                {dog?.name || "Unknown"}
-              </p>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </Spin>
   );
 }
